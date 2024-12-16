96880c575ba778dee9e349b7de127baf37c604b6
sridhar <sridhard05tech@gmail.com>
2024-12-16 16:20:01 +0530

    initial templates

---
+++ employee-managament-system/resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+    This is a About Page
+</x-layout>
---
+++ employee-managament-system/resources/views/contact.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+    This is a Contact Page
+</x-layout>
